80cd7b083712a060e17430532f37cf204a37dd9b
Gabriel Garcia <garciat@uber.com>
2020-05-31 11:17:15 +0200

    Add type hints for mypy --strict

--- gean/__init__.py
+++ gean/__init__.py
@@ -1,5 +1,13 @@
+from abc import ABC, abstractmethod, abstractproperty
 import inspect
-from typing import Any, Dict, Iterable, List, get_type_hints
+from typing import Any, Callable, ClassVar, Dict, Iterable, List, Optional, Tuple, Union, cast, get_type_hints
+
+
+class _Unreachable(Exception): pass
+
+
+def has_custom_init(cls: type) -> bool:
+  return getattr(cls, '__init__', None) is object.__init__
 
 
 def linearize_type_hierarchy(root: type) -> Iterable[type]:
@@ -12,33 +20,20 @@ def linearize_type_hierarchy(root: type) -> Iterable[type]:
       yield from linearize_type_hierarchy(parent)
 
 
-class Resolver:
-  def resolve(self, interface, *, name=None):
-    raise NotImplementedError
+class Resolver(ABC):
+  @abstractmethod
+  def resolve(self, interface: type, *, name: Optional[str] =None) -> Any: ...
 
 
-class Provider:
-  @property
-  def typeof(self):
-    raise NotImplementedError
+class Provider(ABC):
+  @abstractproperty
+  def typeof(self) -> type: ...
 
-  def provide(self, resolver):
-    raise NotImplementedError
-
-
-class InstanceProvider(Provider):
-  def __init__(self, instance):
-    self._instance = instance
-
-  @property
-  def typeof(self):
-    return type(self._instance)
-
-  def provide(self, resolver):
-    return self._instance
+  @abstractmethod
+  def provide(self, resolver: Resolver) -> Any: ...
 
 
-def kwargs_for_annotations(resolver: Resolver, annotations: Dict[str, type]):
+def kwargs_for_annotations(resolver: Resolver, annotations: Dict[str, type]) -> Dict[str, Any]:
   kwargs = {}
 
   for param_name, param_type in annotations.items():
@@ -49,34 +44,56 @@ def kwargs_for_annotations(resolver: Resolver, annotations: Dict[str, type]):
   return kwargs
 
 
+class InstanceProvider(Provider):
+
+  _instance: Any
+
+  def __init__(self, instance: Any):
+    self._instance = instance
+
+  @property
+  def typeof(self) -> type:
+    return type(self._instance)
+
+  def provide(self, resolver: Resolver) -> Any:
+    return self._instance
+
+
 class CallableProvider(Provider):
-  def __init__(self, kallable):
+
+  # _callable: Callable[..., Any]
+  _annotations: Dict[str, type]
+
+  def __init__(self, kallable: Callable[..., Any]):
     self._callable = kallable
     self._annotations = get_type_hints(self._callable)
 
   @property
-  def typeof(self):
+  def typeof(self) -> type:
     return self._annotations['return']
 
-  def provide(self, resolver):
+  def provide(self, resolver: Resolver) -> Any:
     return self._callable(**kwargs_for_annotations(resolver, self._annotations))
 
 
 class ClassProvider(Provider):
-  def __init__(self, cls):
+
+  _cls: type
+
+  def __init__(self, cls: type):
     self._cls = cls
 
   @property
-  def typeof(self):
+  def typeof(self) -> type:
     return self._cls
 
-  def provide(self, resolver):
-    if self._cls.__init__ is object.__init__:
+  def provide(self, resolver: Resolver) -> Any:
+    if has_custom_init(self._cls):
       return self._provide_as_autowired(resolver)
     else:
       return self._provide_as_callable(resolver)
 
-  def _provide_as_autowired(self, resolver):
+  def _provide_as_autowired(self, resolver: Resolver) -> Any:
     annotations = get_type_hints(self._cls)
     instance = self._cls()
 
@@ -85,52 +102,62 @@ class ClassProvider(Provider):
 
     return instance
 
-  def _provide_as_callable(self, resolver):
-    annotations = get_type_hints(self._cls.__init__)
+  def _provide_as_callable(self, resolver: Resolver) -> Any:
+    annotations = get_type_hints(getattr(self._cls, '__init__'))
     return self._cls(**kwargs_for_annotations(resolver, annotations))
 
 
 class ModuleMethodProvider(Provider):
-  def __init__(self, module_cls, unbound_method):
+
+  _module_cls: type
+  # _unbound_method: Callable[..., Any]
+
+  def __init__(self, module_cls: type, unbound_method: Callable[..., Any]):
     self._module_cls = module_cls
     self._unbound_method = unbound_method
 
   @property
-  def typeof(self):
-    return get_type_hints(self._unbound_method)['return']
+  def typeof(self) -> type:
+    return cast(type, get_type_hints(self._unbound_method)['return'])
 
-  def provide(self, resolver):
+  def provide(self, resolver: Resolver) -> Any:
     module = resolver.resolve(self._module_cls)
-    method = getattr(module, self._unbound_method.__name__)
+    method = cast(Callable[..., Any], getattr(module, getattr(self._unbound_method, '__name__')))
     annotations = get_type_hints(method)
     return method(**kwargs_for_annotations(resolver, annotations))
 
 
+class CacheSentinel: pass
+
+
 class CachedProvider(Provider):
-  _UNSET = object()
+
+  _UNSET: ClassVar[CacheSentinel] = CacheSentinel()
+
+  _subject: Provider
+  _instance: Union[CacheSentinel, Any]
 
   def __init__(self, subject: Provider):
     self._subject = subject
     self._instance = self._UNSET
 
   @property
-  def typeof(self):
+  def typeof(self) -> type:
     return self._subject.typeof
 
-  def provide(self, resolver):
+  def provide(self, resolver: Resolver) -> Any:
     if self._instance is self._UNSET:
       self._instance = self._subject.provide(resolver)
-
     return self._instance
 
 
 class DependencyModuleSupport:
   @staticmethod
-  def is_module(cls):
+  def is_module(cls: type) -> bool:
     return inspect.isclass(cls) and cls.__name__.endswith('Module')
 
   @staticmethod
-  def get_methods(cls):
+  def get_methods(cls: type) -> Iterable[Tuple[str, Callable[..., Any]]]:
     for name in dir(cls):
       if name.startswith('_'):
         continue
@@ -140,12 +167,15 @@ class DependencyModuleSupport:
 
 
 class includes:
-  _PROP_NAME = '_includes'
 
-  def __init__(self, *items):
+  _PROP_NAME: ClassVar[str] = '_includes'
+
+  _items: Tuple[type, ...]
+
+  def __init__(self, *items: type):
     self._items = items
 
-  def __call__(self, cls):
+  def __call__(self, cls: type) -> type:
     if not DependencyModuleSupport.is_module(cls):
       raise TypeError('not a module')
 
@@ -154,12 +184,12 @@ class includes:
     return cls
 
   @classmethod
-  def write(cls, target, items):
+  def write(cls, target: type, items: Tuple[type, ...]) -> None:
     setattr(target, cls._PROP_NAME, items)
 
   @classmethod
-  def read(cls, target):
-    return getattr(target, cls._PROP_NAME, [])
+  def read(cls, target: type) -> Tuple[type, ...]:
+    return cast(Tuple[type, ...], getattr(target, cls._PROP_NAME, ()))
 
 
 class ResolutionError(Exception): pass
@@ -168,22 +198,23 @@ class ResolutionError(Exception): pass
 class RegistrationError(Exception): pass
 
 
-class Container:
-  _providers: Dict[type, Dict[str, Provider]]
+class Container(Resolver):
 
-  def __init__(self):
+  _providers: Dict[type, Dict[Optional[str], Provider]]
+
+  def __init__(self) -> None:
     self._providers = {}
 
-  def register_instance(self, instance, *, name=None):
+  def register_instance(self, instance: Any, *, name: Optional[str]=None) -> None:
     return self.register(InstanceProvider(instance), name=name)
 
-  def register_class(self, cls, *, name=None):
+  def register_class(self, cls: type, *, name: Optional[str]=None) -> None:
     return self.register(ClassProvider(cls), name=name)
 
-  def register_callable(self, kallable, *, name=None):
+  def register_callable(self, kallable: Callable[..., Any], *, name: Optional[str]=None) -> None:
     return self.register(CallableProvider(kallable), name=name)
 
-  def register_module(self, cls):
+  def register_module(self, cls: type) -> None:
     self.register(ClassProvider(cls), name=None)
 
     for item in includes.read(cls):
@@ -201,17 +232,17 @@ class Container:
     for name, method in DependencyModuleSupport.get_methods(cls):
       self.register(ModuleMethodProvider(cls, method), name=name)
 
-  def register(self, provider: Provider, *, name=None):
+  def register(self, provider: Provider, *, name: Optional[str]=None) -> None:
     cached_provider = CachedProvider(provider)
     for interface in linearize_type_hierarchy(provider.typeof):
       self._add(interface, name, cached_provider)
 
-  def _add(self, interface, name, provider):
+  def _add(self, interface: type, name: Optional[str], provider: Provider) -> None:
     named_providers = self._providers.setdefault(interface, {})
     if named_providers.setdefault(name, provider) is not provider:
       raise RegistrationError('provider already registered')
 
-  def resolve(self, interface, *, name=None):
+  def resolve(self, interface: type, *, name: Optional[str]=None) -> Any:
     named_providers = self._providers.setdefault(interface, {})
 
     if len(named_providers) == 0:
@@ -220,6 +251,7 @@ class Container:
       # name doesn't matter for unique providers
       for provider in named_providers.values():
         return provider.provide(self)
+      raise _Unreachable
     else:
       if name is None:
         raise ResolutionError('ambiguous dependency interface={!r}'.format(interface))
